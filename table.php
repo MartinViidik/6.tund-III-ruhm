470435b2fcd95348e57a748f9a068bbda29cb0b2
Martin Viidik <martvii@greeny.cs.tlu.ee>
2015-10-07 13:09:55 +0300

    added delete option

--- table.php
+++ table.php
@@ -1,1 +1,48 @@
-siia tuleb tabel
+<?php	
+
+	require_once("functions.php");
+	
+	// kas kustutame midagi ??
+	// ?delete=vastav id mida kustutada on aadressireal
+	if(isset($_GET["delete"])){
+		
+		echo "Kustutame id".$_GET["delete"];
+		// käivitan funktsiooni, saadan kaasa id !!
+		deleteCar($_GET["delete"]);
+	}
+
+	//käivitan funktsiooni
+	$array_of_cars = getCarData();
+	
+	// trükin välja esimese auto
+	//echo $array_of_cars[0]->id." ".$array_of_cars[0]->plate;
+?>
+
+<h2>Tabel</h2>
+<table border=1 >
+	<tr>
+		<th>id</th>
+		<th>user id</th>
+		<th>numbrimärk</th>
+		<th>värv</th>
+		<th>kustuta</th>
+	</tr>
+	
+	<?php
+		// Trükime välja read
+		// massiivi pikkus count()
+		for($i = 0; $i < count($array_of_cars); $i++){
+			//echo $array_of_cars[$i]->id;
+			echo "<tr>";
+			echo "<td>".$array_of_cars[$i]->id."</td>";
+			echo "<td>".$array_of_cars[$i]->user_id."</td>";
+			echo "<td>".$array_of_cars[$i]->plate."</td>";
+			echo "<td>".$array_of_cars[$i]->color."</td>";
+			echo "<td><a href='?delete=".$array_of_cars[$i]->id."'>X</a></td>";
+			echo "</tr>";
+		}
+	
+	?>
+
+</table>
+
